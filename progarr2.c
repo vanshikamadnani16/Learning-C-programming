936ceff858ce77a137b3814310923d610144e9a7
Vanshika Madnani <vanshikamadnani51@gmail.com>
2026-02-21 20:56:23 +0530

    Initial commit

---
+++ progarr2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int arr[3][4];
+    int i,j;
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<4;j++)
+        {
+        scanf("%d", &arr[i][j]);
+        }
+    }
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<4;j++)
+        {
+        printf("%d " ,arr[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
